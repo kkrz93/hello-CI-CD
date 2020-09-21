b077c610dd5d1bbcbb5a9954b04edbab5bc73757
kkrz93 <71643645+kkrz93@users.noreply.github.com>
2020-09-21 16:49:51 +0300

    Update HelloWorldTest.java

--- Java/src/test/java/org/test/HelloWorldTest.java
+++ Java/src/test/java/org/test/HelloWorldTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class HelloWorldTest {
+public class HelloWorldTest {
     
     private HelloWorld helloWorld = new HelloWorld(); 
     
